d32e1eef5f6de59310bfba2af54c5110b5d68e53
ki-hong <rlghd153@gmail.com>
2022-07-09 09:45:36 +0900

    first commit

---
+++ src/main/java/com/swit/SwitApplication.java
@@ -0,0 +1,13 @@
+package com.swit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swit/SwitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
